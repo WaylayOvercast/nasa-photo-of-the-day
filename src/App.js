b5ee20b4fcff32a092cd62c82b6576450f7e2b11
WaylayOvercast <timelyovercast@gmail.com>
2021-10-06 18:22:33 -0500

    added some functionality still wip

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import axios from "axios";
 
 function App() {
 
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
+  const [isOpen, setOpen] = useState(false);
 
   useEffect(() =>{
     axios.get('https://api.nasa.gov/planetary/apod?api_key=ZTjEdHib1QVTaB4qhg6c0uqXsOaf1xKjuxqdGArG')
@@ -18,7 +19,9 @@ function App() {
   
     }).catch(err => console.log(err))}, [])
 
-  function imgPop (data){
+
+
+  function imgPop (){
     return(
       <div className='hd-pop'>
         <img className='hd-img' src={`${data.hdurl}`} ></img>
@@ -26,10 +29,11 @@ function App() {
     );
   }
 
+  const imgGrow = isOpen ? { width:'70rem', Transition:'all 1s' } : null
 
   return (
     <div className="App">
-      <div className='top'>
+      <div className='top' style={isOpen ? {flexDirection:"column", justifyContent:"center", alignItems:"center"} : null}>
         <div className='h-con'>
           <h1 className='title'>{`${data.title}`}</h1>
           <div className='p-con'>
@@ -38,25 +42,25 @@ function App() {
         </div>
         <div className='img-con'>
           <p>{` Posted: ${data.date}  By: ${data.copyright}`}</p>
-          <img src={`${data.url}`}></img>
+          <img src={isOpen ? data.hdurl : data.url} style={imgGrow}></img>
           <div className='nxt-img'>
             <button className='btns btn1'>{`<<`}</button> 
-            <button onClick={() =>imgPop(data)} className='btns btn2'>{`+`}</button>
+            <button onClick={() => setOpen(!isOpen)} className='btns btn2'>{`+`}</button>
             <button className='btns btn3'>{`>>`}</button>
           </div>
         </div>
       </div>
       <div className='bottom'>
         <div className='folderButtons'>
-          <button className='fldrs opened-folder'></button>
-          <button className='fldrs btn-folder2'></button>
           <button className='fldrs btn-folder3'></button>
+          <button className='fldrs btn-folder2'></button>
+          <button className='fldrs btn-folder1'></button>
         </div>
-        <div className='folder tab1'>
+        <div className='folder tab3'>
         </div>
         <div className='folder tab2'>
         </div>
-        <div className='folder tab3'>
+        <div className='folder tab1'>
         </div>
       </div>
     </div>
